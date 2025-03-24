26c99900fbe6b60685d359d723f938d1bdfa6d8d
Bryan Chu <bryan.chu@allhere.com>
2025-03-23 21:18:21 -0400

    hold

--- app/api/study/[id]/route.ts
+++ app/api/study/[id]/route.ts
@@ -1,15 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import _data from "@/ctg-studies.json"; // adjust path if needed
 import { ClinicalTrial } from "@/types/clinicalTrials";
 
 const data = _data as ClinicalTrial[];
 
-export async function GET(
-  _req: Request,
-  { params }: { params: { id: string } },
-) {
-  console.log("params", params);
-  const { id } = params;
+export async function GET(req: NextRequest) {
+  const { id } = req.query;
+  console.log("id", id);
   const match = data.find(
     (trial) =>
       trial.protocolSection.identificationModule.nctId.toLowerCase() ===
